1723d8bfd2318816b86a69731f0e366f99b8cd86
griffint <griffin.tschurwald@students.olin.edu>
2014-09-22 19:35:41 -0400

    First Commit!!
    SO exciting!!!

---
+++ app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/HomeScreen.java
@@ -0,0 +1,51 @@
+package com.example.gtschurwald.mobileproto_chatapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new WelcomeFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/WelcomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.gtschurwald.mobileproto_chatapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gtschurwald on 9/22/2014.
+ */
+
+/**
+ * This is the first fragment that appears by default
+ * it will have a button to navigate to the chat fragment
+ * Good practice in switching fragments!!
+ */
+public class WelcomeFragment extends Fragment {
+    //class is called PlaceholderFragment, although layout is a different name
+    public WelcomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Inflate the fragment view so it fills screen
+        View rootView = inflater.inflate(R.layout.fragment_home_screen, container, false);
+        return rootView;
+    }
+}
