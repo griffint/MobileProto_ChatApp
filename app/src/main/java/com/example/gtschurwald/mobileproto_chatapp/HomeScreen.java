b5954ea0aa2e39e20e5140e4fbffa315efaa3684
griffint <griffin.tschurwald@students.olin.edu>
2014-09-29 02:00:10 -0400

    minor changes

--- app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/HomeScreen.java
+++ app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/HomeScreen.java
@@ -3,6 +3,7 @@ package com.example.gtschurwald.mobileproto_chatapp;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,7 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+import android.widget.Button;
 
 
 public class HomeScreen extends Activity {
@@ -19,12 +20,15 @@ public class HomeScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new WelcomeFragment())   //I want to replace with welcome
-                    .commit();
-        }
+
+        //this is the start chat button on the home activity
+        Button chatStartButton = (Button) findViewById(R.id.chatStartButton);
+
+        //this is code to start new activities
+        Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+        startActivity(intent);
     }
+
 
 
     @Override
