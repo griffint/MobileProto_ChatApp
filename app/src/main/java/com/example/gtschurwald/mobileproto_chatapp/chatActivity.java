1445429ebdf3ce5bc2b7ccef23df5db3fb5eeac3
griffint <griffin.tschurwald@students.olin.edu>
2014-10-02 03:24:47 -0400

    Add generic user
    Clicking Ok in dialog box now adds a generic user to the list.

--- app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/chatActivity.java
+++ app/src/main/java/com/example/gtschurwald/mobileproto_chatapp/chatActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -22,14 +23,17 @@ import java.util.ArrayList;
  */
 public class chatActivity extends Activity {
 
+    //Setting up the Firebase here
+    Firebase myFirebaseRef = new Firebase("https://mobileproto2014.firebaseio.com/chatroom/%5b%5bCHATROOM_ID");
+
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_users);
-        // Below sets up my ArrayAdapter to populate the list of chats going on
-        ListView mainListView;
-        ArrayAdapter mArrayAdapter;
-        ArrayList mNameList = new ArrayList();
 
         // Access the ListView
         mainListView = (ListView) findViewById(R.id.usersListView);
@@ -41,15 +45,11 @@ public class chatActivity extends Activity {
 
         // Set the ListView to use the ArrayAdapter
         mainListView.setAdapter(mArrayAdapter);
-
-        //Setting up the Firebase here
-        Firebase myFirebaseRef = new Firebase("https://mobileproto2014.firebaseio.com/chatroom/%5b%5bCHATROOM_ID");
-
     }
 
-    //Now I need code to populate the users from firebase
 
-    //This
+
+    //This just makes the menu buttons appear
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -63,15 +63,15 @@ public class chatActivity extends Activity {
         switch (item.getItemId()) {
             // action with ID action_refresh was selected
             case R.id.add_user:
-                Toast.makeText(this, "Wanna add a user dawg?", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Wanna add a user?", Toast.LENGTH_SHORT)
                         .show();
 
                 //------------------------------------------------------------
                 //This stuff creates an AlertDialog to enter a username
                 AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
-                alert.setTitle("Add a new user");
-                alert.setMessage("Type in your name here homie");
+                alert.setTitle("Add new user");
+                alert.setMessage("Type in your name here ");
 
                 // Set an EditText view to get user input
                 final EditText input = new EditText(this);
@@ -79,7 +79,10 @@ public class chatActivity extends Activity {
 
                 alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        // Do something with value!
+                        // This should create a new chat room
+                        // Also add that value to the list shown in the ListView
+                        mArrayAdapter.add("shit");
+                        mArrayAdapter.notifyDataSetChanged();
                     }
                 });
 
@@ -104,7 +107,5 @@ public class chatActivity extends Activity {
 
         return true;
     }
-
-
 }
 
